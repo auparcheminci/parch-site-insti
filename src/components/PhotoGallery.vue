f567f4b0307bf642400656f39f17015925821755
ineszarka <zarkaines@gmail.com>
2023-04-29 09:19:04 +0000

    Changing URL on build

--- src/components/PhotoGallery.vue
+++ src/components/PhotoGallery.vue
@@ -21,7 +21,7 @@ export default {
             maxBodyLength: Infinity,
             url: 'https://parch-api-strapi.herokuapp.com/api/photos?populate=*',
             headers: {
-                'Authorization': 'Bearer dc7c879469179f7e950b37a0fe3255a9c6f062d43229f8dc5875688b19c671f96c2abca6b095b20cf686cf2b8bf718df0e51c889b967b2003451ff5971dff6327fb4467919bb3da5736388032a9e47e73ae4fbf6270e3fd86e3398c0f1282676fd1111d2a392472e83e24d2cde78e774b9c4256d0178a4c31d8c4d41af56ce36'
+                'Authorization': `Bearer ${import.meta.env.VITE_RENDER_KEY}`
             }
         }
         axios.request(options).then((response) => {
