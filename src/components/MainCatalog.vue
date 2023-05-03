dd6e7f393b3f33e179fed839e5d08d1185db2b5b
ineszarka <zarkaines@gmail.com>
2023-05-03 09:37:27 +0000

    Changing API URL for main catalog only without filters

--- src/components/MainCatalog.vue
+++ src/components/MainCatalog.vue
@@ -9,7 +9,7 @@ import axios from 'axios'
                         <p>{{ catalog.attributes.Maj }}</p>
                     </div>
                     <div class="link-main">
-                        <a :href="`http://localhost:1337` + catalog.attributes.Document.data.attributes.url"
+                        <a :href="`https://strapi-z4iu.onrender.com` + catalog.attributes.Document.data.attributes.url"
                             target="_blank">Voir le catalogue</a>
                     </div>
                 </div>
@@ -28,9 +28,9 @@ export default {
         const options = {
             method: 'GET',
             maxBodyLength: Infinity,
-            url: 'http://localhost:1337/api/main-catalogs?populate=*',
+            url: 'https://strapi-z4iu.onrender.com/api/main-catalogs?populate=*',
             headers: {
-                'Authorization': 'Bearer d9021206876fa9b96a716c638ca82803a98647a80b81657a0f483a3227f1313138e1754fe19fa4ecca3c402b93a13de6543db152245e5cdfbe4e474820131bfe43c334a8e88bb5773e23567c82fdf6a8ab962a9ed394a2b0181e260f364ac9b9a034c903b852270aab7c58c5e28326b57766a6038aa2e15c678525da3bf2f062'
+                'Authorization': `Bearer ${import.meta.env.VITE_RENDER_KEY}`
             }
         }
         axios.request(options).then((response) => {
