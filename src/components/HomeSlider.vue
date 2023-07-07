67cdca6cc2fb72348138c8825e2897fd1c04efaf
ineszarka <zarkaines@gmail.com>
2023-07-07 07:56:31 +0000

    Slider

--- src/components/HomeSlider.vue
+++ src/components/HomeSlider.vue
@@ -3,7 +3,7 @@ import { VueperSlides, VueperSlide } from 'vueperslides'
 import 'vueperslides/dist/vueperslides.css'
 </script>
 <template>
-<vueper-slides fade :touchable="false" class="no-shadow">
+<vueper-slides autoplay fade :touchable="false" class="no-shadow">
   <vueper-slide
     v-for="(slide, i) in slides"
     :key="i"
@@ -19,10 +19,28 @@ export default {
   data: () => ({
   slides: [
     {
-    title: "C'est la rentrée !",
-    content: "Pour tous les goûts et pour tous les budgets. Equipez vos enfants sans prise de tête pour la rentrée scolaire.<br> Choissisez parmi une sélection d'articles qui feront leur bonheur tout au long de l'année, en boutique ou chez nos revendeurs.",
+    title: "De la variété",
+    content: "Au Parchemin, c'est 9 gammes de produits au large choix, constamment revues et mises à jour. Nous avons à coeur que vous puissiez trouver au même endroit tous les articles de bureau et papeterie dont vous avez besoin.",
     // You can also provide a URL for the image.
-    image: 'https://strapi-z4iu.onrender.com/uploads/main_rentree_cote_ee734dcc5b.jpg',
+    image: 'https://strapi-z4iu.onrender.com/uploads/FAC_4_edited_2454e7ecff.webp',
+    },
+    {
+    title: "De la proximité",
+    content: "Une équipe de professionnel à vos côtés en permanence. Nous répondons à vos questions et vous conseillons sur les bons choix à faire en fonction de vos besoins, pour que vous soyez toujours satisfait de vos achats.",
+    // You can also provide a URL for the image.
+    image: 'https://strapi-z4iu.onrender.com/uploads/MAG_193_edited_c3f69d674e.webp',
+    },
+    {
+    title: "De la qualité",
+    content: "Nous sommes toujours à la recherche de produits plus solide, plus durable pour que vous disposiez des meilleurs outils. Biensure nous nous efforcons de toujours vous les vendre au prix le plus juste.",
+    // You can also provide a URL for the image.
+    image: 'https://strapi-z4iu.onrender.com/uploads/MOB_22_c582494519.webp',
+    },
+    {
+    title: "De la fluidité",
+    content: "Nos entrepôts de grande capacité nous permettent de stocker toute la marchandise nécéssaire pour que vous ne soyez jamais à court des produits dont vous avez besoin. Nous assurons ainsi vos réassorts en permanence.",
+    // You can also provide a URL for the image.
+    image: 'https://strapi-z4iu.onrender.com/uploads/EXP_122_597de4a39e.webp',
     },
   ]
 })
