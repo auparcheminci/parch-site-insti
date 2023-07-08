6788dcda40b4011d5b54f2d2cf30c802e6bff67a
ineszarka <zarkaines@gmail.com>
2023-07-08 10:32:23 +0000

    Removing text from slider on the website

--- src/components/HomeSlider.vue
+++ src/components/HomeSlider.vue
@@ -19,26 +19,26 @@ export default {
   data: () => ({
   slides: [
     {
-    title: "De la variété",
-    content: "Au Parchemin, c'est 9 gammes de produits au large choix, constamment revues et mises à jour. Nous avons à coeur que vous puissiez trouver au même endroit tous les articles de bureau et papeterie dont vous avez besoin.",
+    title: "",
+    content: "",
     // You can also provide a URL for the image.
     image: 'https://strapi-z4iu.onrender.com/uploads/FAC_4_edited_2454e7ecff.webp',
     },
     {
-    title: "De la proximité",
-    content: "Une équipe de professionnel à vos côtés en permanence. Nous répondons à vos questions et vous conseillons sur les bons choix à faire en fonction de vos besoins, pour que vous soyez toujours satisfait de vos achats.",
+    title: "",
+    content: "",
     // You can also provide a URL for the image.
     image: 'https://strapi-z4iu.onrender.com/uploads/MAG_193_edited_c3f69d674e.webp',
     },
     {
-    title: "De la qualité",
-    content: "Nous sommes toujours à la recherche de produits plus solide, plus durable pour que vous disposiez des meilleurs outils. Biensure nous nous efforcons de toujours vous les vendre au prix le plus juste.",
+    title: "",
+    content: "",
     // You can also provide a URL for the image.
     image: 'https://strapi-z4iu.onrender.com/uploads/MOB_22_c582494519.webp',
     },
     {
-    title: "De la fluidité",
-    content: "Nos entrepôts de grande capacité nous permettent de stocker toute la marchandise nécéssaire pour que vous ne soyez jamais à court des produits dont vous avez besoin. Nous assurons ainsi vos réassorts en permanence.",
+    title: "",
+    content: "",
     // You can also provide a URL for the image.
     image: 'https://strapi-z4iu.onrender.com/uploads/EXP_122_597de4a39e.webp',
     },
