187f234a94039c83e199ec8269b2127e5a8a917b
auparcheminci <ines.zarka@auparchemin.ci>
2025-09-12 15:39:45 +0200

    Changes on render

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/', // change to '/myapp/' if hosted in a subfolder
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    historyApiFallback: true // ensures refresh works in dev
-  },
-  build: {
-    cssCodeSplit: true, // separate CSS file
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
+
