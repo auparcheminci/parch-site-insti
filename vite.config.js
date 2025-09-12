fe2b39f9a67a8f0d5c548d350e99450a0dfaf000
auparcheminci <ines.zarka@auparchemin.ci>
2025-09-12 15:03:10 +0200

    Refresh debugging

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // change to '/myapp/' if hosted in a subfolder
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    historyApiFallback: true // ensures refresh works in dev
+  },
+  build: {
+    cssCodeSplit: true, // separate CSS file
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
